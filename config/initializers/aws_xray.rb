25132543c370ceb7b6cbaacc1a0a3e5c85be7bd6
panicboat <panicboat@gmail.com>
2020-12-12 10:26:59 +0900

    Fix Environments

--- config/initializers/aws_xray.rb
+++ config/initializers/aws_xray.rb
@@ -1,5 +1,5 @@
 Rails.application.config.xray = {
-  name: ENV['AWS_ECS_SERVICE'],
+  name: ENV['AWS_ECS_SERVICE_NAME'],
   patch: %I[net_http aws_sdk],
   active_record: true,
   context_missing: 'LOG_ERROR'
