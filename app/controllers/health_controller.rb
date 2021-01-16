96196ca67dd5a0d435cf788ad075ee8dc9ef8dda
panicboat <panicboat@gmail.com>
2021-01-17 01:54:43 +0900

    Update health_controller.rb

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,4 +1,4 @@
-class HealthController < ApplicationController
+class HealthController < Panicboat::ApplicationController
   def index
     run Health::Operation::Index, params: params do |ctx|
       render json: represent(Health::Representer::Index, ctx)
